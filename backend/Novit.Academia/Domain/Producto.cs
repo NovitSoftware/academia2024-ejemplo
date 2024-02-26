1caa2858f2a47a8aa4a2905a75208bca0af7e852
jvelazquez <jonathanvgms@gmail.com>
2024-02-26 19:51:51 -0300

    Add api project

---
+++ backend/Novit.Academia/Domain/Producto.cs
@@ -0,0 +1,12 @@
+﻿namespace Novit.Academia.Domain;
+
+public class Producto
+{
+    public int IdProducto { get; set; }
+    public required string Nombre { get; set; }
+    public string? Descripcion { get; set; }
+    public required decimal Precio { get; set; }
+    public string? Imagen { get; set; }
+    public int Stock { get; set; } = 0;
+    public List<ItemCarrito> Items { get; set; } = [];
+}
